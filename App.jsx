3af49da742e527cf1f328ce6b7d02f1bfa88bfa9
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-04-13 16:26:31 -0600

    first commit

---
+++ App.jsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <h1>Hey this is sigfea</h1>
+  );
+};
+
+export default App;
---
+++ main.jsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client';
+// se importa el componente del archivo App.jsx
+import App from "./App";
+
+// se guarda el div con el id: app de nuestro HTML en una variable llamada 'root'
+const root = createRoot(document.getElementById('app'));
+
+// se renderiza el componente importado
+root.render(<App />);
