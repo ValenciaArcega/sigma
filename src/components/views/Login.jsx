7b2f87559efe76ead757724f37bf2d1e10a93fc5
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-04-14 20:46:53 -0600

    features: login screen had been completed, also the wrong credentials message and state is set to can be modified, also some style issues

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,17 +1,26 @@
+import BadCredentials from "../messages/BadCredentials";
+import { useState } from "react";
+
 const Login = () => {
+  const [isWrong, setIsWrong] = useState(true);
+
   return (
     <section className="container-login">
+
+      {isWrong ? <BadCredentials /> : null}
+
       <form className="login">
         <img className="login-img" src="./login.svg" alt="" />
         <h1 className="login-title">Inicia Sesión</h1>
 
         <label className="login-label" htmlFor="inputMail">Correo electrónico</label>
-        <input autoComplete="new-password" className="login-inputMail" type="text" id="inputMail" />
+        <input autoComplete="new-password" className="login-inputMail" type="text" id="inputMail" placeholder="Ingresa tu correo" />
 
         <label className="login-label" htmlFor="inputPass">Contraseña</label>
-        <input className="login-inputPass" type="password" id="inputPass" />
+        <input className="login-inputPass" type="password" id="inputPass" placeholder="Ingresa tu contraseña" />
 
-        <button className="login-buttonLogin">Entrar</button>
+        <button className="login-buttonLogin" type="submit">Entrar</button>
+        <label className="login-labelBGR" htmlFor="lbgr">¿No tienes una cuenta? <button className="login-buttonGoRegister" id="lbgr">Registrate</button></label>
       </form>
     </section>
   );
