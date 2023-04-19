b0509423e2238d0672b544b15f24b946c1fc4a3b
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-04-18 21:23:46 -0600

    features: dashboard view had been addeed and also some style issues

--- src/components/views/Dashboard.jsx
+++ src/components/views/Dashboard.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+import { dashboardCards } from "../../data/dashboard";
+
 const Dashboard = () => {
+  const [fecha, setFecha] = useState('Sin fecha reciente');
+
   return (
-    <h1>I am the main screen of home, the dashboard</h1>
+    <section className="container-dashboard">
+      <header className="dashboard-header"><h1>Mis tiempos</h1></header>
+
+      <main className="dashboard-main">
+        {dashboardCards.map((el, i) => {
+          return (
+            <div className="dashboard-card" key={i}>
+              <h4 className="dashboard-card-title">{el.name}</h4>
+              <p className="dashboard-card-description">{el.description}</p>
+              <h3 className="dashboard-card-date"><span className="gradient">{fecha}</span></h3>
+            </div>
+          );
+        })}
+      </main>
+    </section>
   );
 };
 
